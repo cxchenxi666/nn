c059c13a444affbfdf47d9cb73956314e593440d
cxchenxi666 <11525668@qq.com>
2025-06-12 17:06:32 +0800

    补充注释

--- src/chap04_simple_neural_network/tutorial_minst_fnn-numpy-exercise.py
+++ src/chap04_simple_neural_network/tutorial_minst_fnn-numpy-exercise.py
@@ -304,6 +304,7 @@ with tf.GradientTape() as tape:
     loss = tf.reduce_sum(label * log_prob)
     # 计算负对数似然损失(Negative Log Likelihood Loss)
     grads = tape.gradient(loss, [W1, W2])
+    # 打印TensorFlow计算的梯度（基准值）
     print("W1 Gradient Check:", grads[0].numpy())
     print("W2 Gradient Check:", grads[1].numpy())
 
