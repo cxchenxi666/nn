e77471efa73320b256fcc3501c92d32971695a82
cxchenxi666 <11525668@qq.com>
2025-06-13 14:36:28 +0800

    补充注释

--- src/chap05_CNN/tutorial_random_filter.py
+++ src/chap05_CNN/tutorial_random_filter.py
@@ -6,7 +6,7 @@
 # 导入必要的库
 import os
 import tensorflow as tf
-from tensorflow import keras
+from tensorflow import keras # 引入Keras高级API接口
 from tensorflow.keras import layers, optimizers, datasets
 from tensorflow.keras.layers import Dense, Dropout, Flatten
 from tensorflow.keras.layers import Conv2D, MaxPooling2D
